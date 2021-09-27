957bcc0f76dde47b08a30bb4e3daf39e79a0aade
Ngo Thanh Nam <ngothanhnam2016@gmail.com>
2021-09-27 11:28:03 +0700

    Image Classification with pre-trained networks

---
+++ pyimagesearch/config.py
@@ -0,0 +1,13 @@
+import torch
+
+IMAGE_SIZE = 224
+
+# specify ImageNet mean and standard deviation
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+
+# determine the device 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# specify path to the ImageNet labels
+IN_LABELS = "ilsvrc2012_wordnet_lemmas.txt"
